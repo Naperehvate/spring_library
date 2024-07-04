e2858288d4ef7f8f5eeaac1e476e3e2880f44777
Naperehvate <tropikano647@mail.ru>
2024-07-04 13:04:13 +0500

    Add and remove classes

--- src/main/java/org/example/spring_library/BookController.java
+++ src/main/java/org/example/spring_library/BookController.java
@@ -1,57 +1,53 @@
 package org.example.spring_library;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
-@RestController                                 //Анотация что это контроллер
-@RequestMapping("/api/books")                 //Анотация для маршрутизации
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
-    @Autowired                                   //Анотация для внедрения зависимостей
+    @Autowired
     private IBookRepository bookRepository;
 
     @GetMapping
-    public List<Book> getBooks() {
-        return bookRepository.findAll();
+    public String getAllBooks(Model model) {
+        model.addAttribute("index", bookRepository.findAll());
+        return "index";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Book> GetBookById(@PathVariable Long id) {
-        Optional<Book> book = bookRepository.findById(id);
-        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "create-book";
     }
 
     @PostMapping
-    public ResponseEntity<Book> CreateBook(@RequestBody Book book)
-    {
-       return ResponseEntity.ok(bookRepository.save(book));
+    public String createBook(@RequestBody Book book) {
+        bookRepository.save(book);
+        return "redirect:/index";
     }
 
-    @PostMapping("/{id}")
-    public ResponseEntity<Book> UpdateBook(@PathVariable Long id, @RequestBody Book updatedBook)
-    {
-       Optional<Book> book = bookRepository.findById(id);
-       if (book.isPresent()) {
-           book.get().setTitle(updatedBook.getTitle());
-           book.get().setAuthor(updatedBook.getAuthor());
-           return ResponseEntity.ok(bookRepository.save(book.get()));
-       } else {
-           return ResponseEntity.notFound().build();
-       }
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid book id: " + id));
+        model.addAttribute("book", book);
+        return "edit-book";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Book> DeleteBook(@PathVariable Long id) {
-        Optional<Book> book = bookRepository.findById(id);
-        if (book.isPresent()) {
-            bookRepository.delete(book.get());
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @PutMapping("/update/{id}")
+    public String updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid book id: " + id));
+        book.setTitle(updatedBook.getTitle());
+        book.setAuthor(updatedBook.getAuthor());
+        bookRepository.save(book);
+        return "redirect:/index";
     }
 
+    @DeleteMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid book id: " + id));
+        bookRepository.delete(book);
+        return "redirect:/index";
+    }
 }
